abbadba5e5458001314b11e7b20f7c762aabd212
pedroth <pedrotiago92@gmail.com>
2023-10-22 10:53:24 +0100

    fix parser import in worker

--- worker.js
+++ worker.js
@@ -1,4 +1,7 @@
-import { parse } from "/dist/web/Parser.js";
+const isGithub = window.location.host === "pedroth.github.io";
+const NABLA_WORD = isGithub ? "/nabladown.js" : ""
+
+const { parse } = await import(NABLA_WORD + "/dist/web/Parser.js");
 
 onmessage = e => {
   console.log("Worker: Message received from main script", e);
