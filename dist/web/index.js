8b6ae38837a9664cd658c430256782e428f0bab2
pedroth <pedrotiago92@gmail.com>
2025-10-26 18:29:01 +0000

    update version

--- dist/web/index.js
+++ dist/web/index.js
@@ -63456,7 +63456,7 @@ function trimPreserveNewlines(str) {
 var import_lib = __toESM(require_lib(), 1);
 var es_default = import_lib.default;
 // package.json
-var version = "4.0.4";
+var version = "4.0.5";
 
 // src/CodeRender/CodeRender.js
 function render4(tree) {
