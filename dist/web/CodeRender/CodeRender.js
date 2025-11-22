7a67d23df45f76e273a87df2ea9ae269ce3fc419
pedroth <pedrotiago92@gmail.com>
2025-11-22 21:30:06 +0000

    update

--- dist/web/CodeRender/CodeRender.js
+++ dist/web/CodeRender/CodeRender.js
@@ -63462,7 +63462,7 @@ function trimPreserveNewlines(str) {
 var import_lib = __toESM(require_lib(), 1);
 var es_default = import_lib.default;
 // package.json
-var version = "4.0.5";
+var version = "4.0.6";
 
 // src/CodeRender/CodeRender.js
 function render4(tree) {
