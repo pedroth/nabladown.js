0cd1c2bb6d0d3a47df6c9eba24bcdb5bd02be62e
pedroth <pedrotiago92@gmail.com>
2026-05-20 23:36:18 +0100

    bump version to 4.1.4 in package.json files

--- dist/web/index.js
+++ dist/web/index.js
@@ -63518,7 +63518,7 @@ function trimPreserveNewlines(str) {
 var import_lib = __toESM(require_lib(), 1);
 var es_default = import_lib.default;
 // package.json
-var version = "4.1.3";
+var version = "4.1.4";
 
 // src/CodeRender/CodeRender.js
 function render4(tree) {
